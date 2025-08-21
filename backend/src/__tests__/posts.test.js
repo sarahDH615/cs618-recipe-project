93417fcff259da2ed2f79759ff38d661853bcf7b
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-08-21 00:29:35 +0000

    feat: listing posts, tests

--- backend/src/__tests__/posts.test.js
+++ backend/src/__tests__/posts.test.js
@@ -1,10 +1,44 @@
 import mongoose from 'mongoose'
-import { describe, expect, test } from '@jest/globals'
-import { createPost } from '../services/posts.js'
+import { describe, expect, test, beforeEach } from '@jest/globals'
 import { Post } from '../db/models/post.js'
+import {
+  createPost,
+  listAllPosts,
+  listPostsByAuthor,
+  listPostsByTag,
+} from '../services/posts.js'
 
-// test 1: creating posts
+// creating sample data
+const samplePosts = [
+  { title: 'Learning Redux', author: 'Daniel Bugl', tags: ['redux'] },
+  { title: 'Learn React Hooks', author: 'Daniel Bugl', tags: ['react'] },
+  {
+    title: 'Full-Stack React Projects',
+    author: 'Daniel Bugl',
+    tags: ['react', 'nodejs'],
+  },
+  { title: 'Guide to TypeScript' },
+]
+
+let createdSamplePosts = []
+
+beforeEach(async () => {
+  // delete any posts in the database
+  await Post.deleteMany({})
+  // define locally ?
+  createdSamplePosts = []
+  // iterate through sample data
+  for (const post of samplePosts) {
+    // create a Post instance from each sample post
+    const createdPost = new Post(post)
+    // save to db and push to array
+    // using mongoose save functionality instead of createPost() to make sure tests are modular
+    createdSamplePosts.push(await createdPost.save())
+  }
+})
+
 describe('creating posts', () => {
+  // test 1: creating posts with all params
   test('with all parameters should succeed', async () => {
     const post = {
       title: 'Hello Mongoose!',
@@ -46,5 +80,47 @@ describe('creating posts', () => {
     }
     const createdPost = await createPost(post)
     expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
+  })
+})
+
+describe('listing posts', () => {
+  // test 1: list all
+  test('should return all posts', async () => {
+    const posts = await listAllPosts()
+    expect(posts.length).toEqual(createdSamplePosts.length)
+  })
+  // test 2: sorting
+  test('should return posts sorted by creation date descending by default', async () => {
+    const posts = await listAllPosts()
+    // manual sort of createdSamplePosts
+    const sortedSamplePosts = createdSamplePosts.sort(
+      (a, b) => b.createdAt - a.createdAt,
+    )
+    expect(posts.map((post) => post.createdAt)).toEqual(
+      sortedSamplePosts.map((post) => post.createdAt),
+    )
+  })
+  // test 3: custom sort
+  test('should take into account provided sorting options', async () => {
+    const posts = await listAllPosts({
+      sortBy: 'updatedAt',
+      sortOrder: 'ascending',
+    })
+    const sortedSamplePosts = createdSamplePosts.sort(
+      (a, b) => a.createdAt - b.createdAt,
+    )
+    expect(posts.map((post) => post.updatedAt)).toEqual(
+      sortedSamplePosts.map((post) => post.updatedAt),
+    )
+  })
+  // test 4: filter by author
+  test('should be able to filter posts by author', async () => {
+    const posts = await listPostsByAuthor('Daniel Bugl')
+    expect(posts.length).toBe(3)
+  })
+  // test 5: filter by tags
+  test('should be able to filter posts by tag', async () => {
+    const posts = await listPostsByTag('nodejs')
+    expect(posts.length).toBe(1)
   })
 })
--- backend/src/services/posts.js
+++ backend/src/services/posts.js
@@ -4,3 +4,23 @@ export async function createPost({ title, author, contents, tags }) {
   const post = new Post({ title, author, contents, tags })
   return await post.save()
 }
+
+// list posts according on query passed in with sorting
+async function listPosts(
+  query = {},
+  { sortBy = 'createdAt', sortOrder = 'descending' } = {},
+) {
+  return await Post.find(query).sort({ [sortBy]: sortOrder })
+}
+// list all posts by calling listPosts() with empty query
+export async function listAllPosts(options) {
+  return await listPosts({}, options)
+}
+// list posts by author by passing in author as query to listPosts()
+export async function listPostsByAuthor(author, options) {
+  return await listPosts({ author }, options)
+}
+// list posts by tags by passing in tags as query to listPosts()
+export async function listPostsByTag(tags, options) {
+  return await listPosts({ tags }, options)
+}
